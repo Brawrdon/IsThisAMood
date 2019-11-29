404469b75d6b0ceef9c2cbc7b04b5bf2fd159e98
Brawrdon <BrandonOkeke@icloud.com>
2019-11-29 20:39:47 +0000

    Initial commit

---
+++ server/Controllers/AlexaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IsThisAMood.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlexaController : ControllerBase
+    {
+        private readonly ILogger<AlexaController> _logger;
+
+        public AlexaController(ILogger<AlexaController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpPost]
+        public ActionResult<SkillResponse> ReceiveRequest(SkillRequest skillRequest)
+        {
+            var finalResponse = ResponseBuilder.Tell(new PlainTextOutputSpeech("I got your request."));
+            return finalResponse;
+        }
+    }
+}
---
+++ server/Middlewares/AlexaRequestValidationMiddleware.cs
@@ -0,0 +1,105 @@
+using Alexa.NET.Request;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+
+namespace IsThisAMood.Middlewares
+{
+    /// <summary>
+    /// An ASP.NET Core Middleware for validating Alexa requests
+    /// </summary>
+    public class AlexaRequestValidationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<AlexaRequestValidationMiddleware> _logger;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="next"></param>
+        public AlexaRequestValidationMiddleware(RequestDelegate next, ILogger<AlexaRequestValidationMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Validate if all necessary parts for a valid request are available
+        /// and pass them to the RequestsVerification tool
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task Invoke(HttpContext context)
+        {
+            
+            _logger.LogInformation("Inside Middleware");
+            
+            // EnableRewind so the body can be read without causing issues to the request pipeline
+            context.Request.EnableBuffering();
+            
+            // Verify SignatureCertChainUrl is present
+            context.Request.Headers.TryGetValue("SignatureCertChainUrl", out var signatureChainUrl);
+            if (string.IsNullOrWhiteSpace(signatureChainUrl))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            Uri certUrl;
+            try
+            {
+                certUrl = new Uri(signatureChainUrl);
+            }
+            catch
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            // Verify SignatureCertChainUrl is Signature
+            context.Request.Headers.TryGetValue("Signature", out var signature);
+            if (string.IsNullOrWhiteSpace(signature))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            var body = await new StreamReader(context.Request.Body).ReadToEndAsync();
+            context.Request.Body.Position = 0;
+
+            if (string.IsNullOrWhiteSpace(body))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+            var valid = await RequestVerification.Verify(signature, certUrl, body);
+            if (!valid)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+    
+    /// <summary>
+    /// Middleware builder
+    /// </summary>
+    public static class AlexaRequestValidationMiddlewareExtension
+    {
+        /// <summary>
+        /// Add AlexaRequestValidationMiddleware to the request pipeline
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseAlexaRequestValidation(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AlexaRequestValidationMiddleware>();
+        }
+    }
+}
+
