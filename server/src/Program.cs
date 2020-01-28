7c0cddc74b1afaab0599108a409f800c84734147
Brawrdon <BrandonOkeke@icloud.com>
2020-01-28 14:28:32 +0000

    feat(Alexa): Add Serilog package

--- server/src/Program.cs
+++ server/src/Program.cs
@@ -1,13 +1,37 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace IsThisAMood
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -18,6 +42,7 @@ namespace IsThisAMood
                     {
                         config.ListenLocalhost(6000);
                     }).UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }
